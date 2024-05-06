22ba4bdd08c66556287df74cbed6794b1033eb5a
sergio noriega cantillo <sergionoriegacantillo@gmail.com>
2024-05-06 15:01:32 +0200

    .

--- password_strenght_checker.py
+++ password_strenght_checker.py
@@ -39,7 +39,7 @@ class Create:
         self.letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
         self.letters_Uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
         self.numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-        self.symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+        self.symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+',"@"]
         self.password_list = []
         self.password = ""
 
@@ -138,12 +138,13 @@ class checker(Create):
             print("the lenght of your password isn´t ok must be with 12 caracters")
 
     def main(self):
-            self.get_password()
-            self.checkTheuppercase()
-            self.checkThelowecase()
-            self.checkTheNumber()
-            self.checkThesymbol()
-            self.checktheStrenght()
+            while True:
+                self.get_password()
+                self.checkTheuppercase()
+                self.checkThelowecase()
+                self.checkTheNumber()
+                self.checkThesymbol()
+                self.checktheStrenght()
             
         
         
